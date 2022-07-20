e127ecb39c82e1acfe415ee26a59671336294227
Alejo Aizpitarte <creepgg@hotmail.com>
2022-07-19 21:02:55 -0300

    GitHub Pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { animation, style } from '@angular/animations';
+import { NONE_TYPE } from '@angular/compiler';
+import { Component, Input, OnInit } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'SPAPresentacion';
+  background: boolean=true;
+  DOM: boolean=false;
+  NombreUsuario: any;
+  button:any;
+  formulario:any;
+  
+
+  
+  
+  
+  
+  public async getInputValue(inputValue:string){
+    console.log(inputValue);
+     this.NombreUsuario = (inputValue);
+    
+  
 }
+
+  Ocultar() {
+    if (this.getInputValue){
+        this.background=false;
+        this.DOM=true;
+        this.getInputValue;
+        
+        
+}
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatIconModule} from '@angular/material/icon';
+
+
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    LayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
